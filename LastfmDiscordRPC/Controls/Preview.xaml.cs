1e2092759b71211a40726d36727a9c6b4e450231
RegorForgotTheirPasssword <muhammedrahman2005@gmail.com>
2022-07-14 13:08:50 +0300

    Final commit for today - fixed up a duplicate image

--- LastfmDiscordRPC/Controls/Preview.xaml.cs
+++ LastfmDiscordRPC/Controls/Preview.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace LastfmDiscordRPC.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace LastfmDiscordRPC.Controls;
 
 public partial class Preview
 {
