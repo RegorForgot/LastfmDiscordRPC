5ffbf6bdfa586b692178645a940a65ad03d8c853
RegorForgotTheirPasssword <muhammedrahman2005@gmail.com>
2022-07-19 02:56:58 +0300

    naming convention + interpolated

--- LastfmDiscordRPC/Models/DiscordClient.cs
+++ LastfmDiscordRPC/Models/DiscordClient.cs
@@ -79,10 +79,12 @@ public class DiscordClient : IDisposable
                 smallText = "Stopped.";
             }
         }
-        string formatted_scrobbles = String.Format("{0:n0}", int.Parse(response.Playcount));
+        
+        
+        string formattedScrobbles = $"{int.Parse(response.Playcount):n0}";
         Button button = new Button
         {
-            Label = $"{formatted_scrobbles} scrobbles", Url = @$"https://www.last.fm/user/{username}/"
+            Label = $"{formattedScrobbles} scrobbles", Url = @$"https://www.last.fm/user/{username}/"
         };
 
         _presence = new RichPresence
