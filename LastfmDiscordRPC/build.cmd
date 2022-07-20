bb60a4812d352cc871392e9a8a40d9829580f991
RegorForgotTheirPasssword <muhammedrahman2005@gmail.com>
2022-07-20 13:01:29 +0300

    minor change

--- LastfmDiscordRPC/build.cmd
+++ LastfmDiscordRPC/build.cmd
@@ -10,3 +10,4 @@ rename "..\Release\LastfmDiscordRPC.exe" "LastfmDiscordRPC-SelfContained-x64.exe
 dotnet publish -r win-x64 -c Release --self-contained false -o=..\Release /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishSingleFile=true
 del "..\Release\LastfmDiscordRPC-x64.exe"
 rename "..\Release\LastfmDiscordRPC.exe" "LastfmDiscordRPC-x64.exe"
+del "..\Release\LastfmDiscordRPC.pdb"
